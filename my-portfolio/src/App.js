5fb5d9a4a08167f27c9a086f856c7a71245ae9a9
DESKTOP-97DRD86\Hi <bernardyuzhe9@gmail.com>
2024-03-15 01:50:00 +0800

    Card update

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './Component/Component.css';
 import Navbar from './Navbar'
+import SideNav from './SideNav';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SideNav/>
     </div>
   );
 }
