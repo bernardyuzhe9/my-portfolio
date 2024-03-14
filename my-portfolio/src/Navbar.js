8901ea46440ac80e7343abbe18543b21b3534159
DESKTOP-97DRD86\Hi <bernardyuzhe9@gmail.com>
2024-03-15 00:26:13 +0800

    Header

--- my-portfolio/src/Navbar.js
+++ my-portfolio/src/Navbar.js
@@ -23,15 +23,15 @@ const callsToAction = [
   { name: 'Contact sales', href: '#', icon: PhoneIcon },
 ]
 
-function Navbar(...classes) {
+function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false); // Moved useState inside the functional component
 
   return (
-    <header className="bg-white">
+<header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
@@ -198,5 +198,7 @@ export default function Navbar() {
         </Dialog.Panel>
       </Dialog>
     </header>
-  )
+   );
 }
+ 
+export default Navbar;
