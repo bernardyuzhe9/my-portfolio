42acb19a6e5485a9c266dd96d42335833c5aa4dd
bernardyuzhe9 <97150285+bernardyuzhe9@users.noreply.github.com>
2024-03-26 15:56:35 +0800

    update

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './Component/Component.css';
 import Navbar from './Navbar'
 import SideNav from './SideNav';
+import BentoGridDemo from './BentoGridDemo'; 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <SideNav/>
+      <BentoGridDemo/>
     </div>
   );
 }
--- my-portfolio/src/SideNav.js
+++ my-portfolio/src/SideNav.js
@@ -34,7 +34,7 @@ const SideNav = () => {
 
 const SideNavGroup = () => {
     return (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div>
                 <SideNav />
             </div>
